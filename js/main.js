0f33f53ab901d878d2bbb072a888d10ee637a1bb
Владислав Плотников <gr436_plval@lab-203-3.tomtit-tomsk.ru>
2026-02-09 12:09:20 +0700

    finish

--- js/main.js
+++ js/main.js
@@ -1,14 +1,132 @@
+Vue.component('note-card', {
+    props: {
+        note: {
+            type: Object,
+            required: true
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
+    },
+    template: `
+        <div class="card" :class="{ 'card-finished': completed }">
+            <div class="card-title">{{ note.title }}</div>
+            <div class="card-items">
+                <div v-for="(item, idx) in note.items" :key="idx" class="check-item">
+                    <input 
+                        type="checkbox" 
+                        v-model="item.checked" 
+                        :disabled="disabled || completed"
+                        @change="onCheck"
+                        :id="uniqueId(idx)"
+                    >
+                    <label :for="uniqueId(idx)" :class="{ 'strike': completed }">{{ item.text }}</label>
+                </div>
+            </div>
+            <div v-if="completed" class="timestamp">
+                END_TIME: {{ note.completedAt }}
+            </div>
+        </div>
+    `,
+    methods: {
+        onCheck() {
+            this.$emit('update-progress');
+        },
+        uniqueId(idx) {
+            return 'chk-' + this.note.id + '-' + idx;
+        }
+    }
+});
 
+Vue.component('note-form', {
+    props: {
+        isBlocked: { type: Boolean, required: true },
+        col1Count: { type: Number, required: true }
+    },
+    data() {
+        return {
+            newTitle: '',
+            newTodos: [
+                { text: '', checked: false },
+                { text: '', checked: false },
+                { text: '', checked: false }
+            ]
+        }
+    },
+    computed: {
+        isFormValid() {
+            const filledTodos = this.newTodos.filter(t => t.text.trim() !== '');
+            return this.newTitle.trim() !== '' && filledTodos.length >= 3 && filledTodos.length <= 5;
+        }
+    },
+    template: `
+        <div class="control-panel">
+            <div class="panel-header">CREATE_NEW_TASK</div>
+            
+            <div class="form-body">
+                <input type="text" v-model="newTitle" placeholder="> ENTER TITLE..." class="input-title">
+                
+                <div class="todo-list">
+                    <div v-for="(todo, index) in newTodos" :key="index" class="input-row">
+                        <span class="prefix">[ ]</span>
+                        <input type="text" v-model="todo.text" placeholder="TASK ITEM...">
+                        <button @click="removeTodoInput(index)" v-if="newTodos.length > 3" class="btn-del">X</button>
+                    </div>
+                </div>
+
+                <div class="actions">
+                    <button @click="addTodoInput" v-if="newTodos.length < 5" class="btn-secondary">+ ADD ITEM</button>
+                    
+                    <button 
+                        @click="submitNote" 
+                        class="btn-primary"
+                        :disabled="!isFormValid || col1Count >= 3 || isBlocked"
+                    >
+                        <span v-if="isBlocked">System Blocked</span>
+                        <span v-else-if="col1Count >= 3">Col_1 Full</span>
+                        <span v-else>EXECUTE ADD</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    `,
+    methods: {
+        addTodoInput() {
+            if (this.newTodos.length < 5) this.newTodos.push({ text: '', checked: false });
+        },
+        removeTodoInput(index) {
+            if (this.newTodos.length > 3) this.newTodos.splice(index, 1);
+        },
+        submitNote() {
+            if (this.isFormValid) {
+                const newNote = {
+                    id: Date.now(),
+                    title: this.newTitle,
+                    items: this.newTodos.filter(t => t.text.trim() !== ''),
+                    completedAt: null
+                };
+                
+                this.$emit('create-note', newNote);
+                
+                this.newTitle = '';
+                this.newTodos = [
+                    { text: '', checked: false },
+                    { text: '', checked: false },
+                    { text: '', checked: false }
+                ];
+            }
+        }
+    }
+});
 
 new Vue({
     el: '#app',
     data: {
-        newTitle: '',
-        newTodos: [
-            { text: '', checked: false },
-            { text: '', checked: false },
-            { text: '', checked: false }
-        ],
         col1: [],
         col2: [],
         col3: []
@@ -27,47 +145,23 @@ new Vue({
         col3: { handler: 'saveData', deep: true }
     },
     computed: {
-        isFormValid() {
-            const filledTodos = this.newTodos.filter(t => t.text.trim() !== '');
-            return this.newTitle.trim() !== '' && filledTodos.length >= 3 && filledTodos.length <= 5;
-        },
         isBlocked() {
             const isCol2Full = this.col2.length >= 5;
             const hasPendingCard = this.col1.some(note => {
                 const checkedCount = note.items.filter(i => i.checked).length;
                 return (checkedCount / note.items.length) * 100 > 50;
             });
-            
             return isCol2Full && hasPendingCard;
         }
     },
     methods: {
-        addTodoInput() {
-            if (this.newTodos.length < 5) this.newTodos.push({ text: '', checked: false });
-        },
-        removeTodoInput(index) {
-            if (this.newTodos.length > 3) this.newTodos.splice(index, 1);
-        },
-        addNote() {
-            if (this.isFormValid && this.col1.length < 3 && !this.isBlocked) {
-                const newNote = {
-                    id: Date.now(),
-                    title: this.newTitle,
-                    items: this.newTodos.filter(t => t.text.trim() !== ''),
-                    completedAt: null
-                };
-                
+        addNote(newNote) {
+            if (this.col1.length < 3 && !this.isBlocked) {
                 this.col1.push(newNote);
-                
-                this.newTitle = '';
-                this.newTodos = [
-                    { text: '', checked: false },
-                    { text: '', checked: false },
-                    { text: '', checked: false }
-                ];
             }
         },
-        checkStatus(note, currentColumn) {
+
+        handleCardUpdate(note, currentColumn) {
             const total = note.items.length;
             const checked = note.items.filter(i => i.checked).length;
             const percentage = (checked / total) * 100;
@@ -81,10 +175,11 @@ new Vue({
                 if (percentage > 50) {
                     if (this.col2.length < 5) {
                         this.moveNote(note, this.col1, this.col2);
-                    } 
+                    }
                 }
             }
         },
+
         moveNote(note, fromArr, toArr) {
             const index = fromArr.indexOf(note);
             if (index !== -1) {
@@ -92,6 +187,7 @@ new Vue({
                 toArr.push(note);
             }
         },
+
         moveToCol3(note, fromColNum) {
             const now = new Date();
             note.completedAt = now.toLocaleString(); 
@@ -99,6 +195,7 @@ new Vue({
             if (fromColNum === 1) this.moveNote(note, this.col1, this.col3);
             if (fromColNum === 2) this.moveNote(note, this.col2, this.col3);
         },
+
         saveData() {
             const data = {
                 col1: this.col1,
